d01ff4068691f7d75824952075a50efe52b0659e
Fazin <grahamderson1@gmail.com>
2024-03-24 02:16:57 -0400

    move steam libs and change debugsetup

--- Build/debugsetup.py
+++ Build/debugsetup.py
@@ -1,4 +1,4 @@
 import shutil
 
-shutil.copyfile("steam_api.dll", "bin/Debug/net6.0/runtimes/win-x64/native/steam_api.dll")
-shutil.copyfile("libsteam_api.so", "bin/Debug/net6.0/runtimes/linux-x64/native/libsteam_api.so")
+shutil.copyfile("Build/steam_api.dll", "bin/Debug/net6.0/runtimes/win-x64/native/steam_api.dll")
+shutil.copyfile("Build/libsteam_api.so", "bin/Debug/net6.0/runtimes/linux-x64/native/libsteam_api.so")
