6058f219bbbb1f91d5831b486437570ddee78068
Fazin <grahamderson1@gmail.com>
2024-04-12 00:29:46 -0400

    fix debugsetup.js

--- Build/debugsetup.js
+++ Build/debugsetup.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
-fs.copyFile('Build/steam_api.dll', 'bin/Debug/net6.0/runtimes/win-x64/native/steam_api.dll', err => {
+fs.copyFile('steam_api64.dll', 'bin/Debug/net8.0/runtimes/win-x64/native/steam_api.dll', err => {
     if (err) throw err;
-    console.log('Copied Build/steam_api.dll to bin/Debug/net6.0/runtimes/win-x64/native/steam_api.dll');
+    console.log('Copied steam_api64.dll to bin/Debug/net8.0/runtimes/win-x64/native/steam_api64.dll');
 });
 
-fs.copyFile('Build/libsteam_api.so', 'bin/Debug/net6.0/runtimes/linux-x64/native/libsteam_api.so', err => {
+fs.copyFile('libsteam_api.so', 'bin/Debug/net8.0/runtimes/linux-x64/native/libsteam_api.so', err => {
     if (err) throw err;
-    console.log('Copied Build/libsteam_api.so to bin/Debug/net6.0/runtimes/linux-x64/native/libsteam_api.so');
+    console.log('Copied libsteam_api.so to bin/Debug/net8.0/runtimes/linux-x64/native/libsteam_api.so');
 });
